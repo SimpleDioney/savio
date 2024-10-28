ef4821f99d2f093fb3a79f4f1bf90129c1050712
Dioney Gabriel <62361235+SimpleDioney@users.noreply.github.com>
2024-10-28 20:12:57 -0300

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
     script: "./server.js",
     instances: 1,
     autorestart: true,
-    watch: false, // Desativamos o watch mode para evitar reinícios constantes
+    watch: false, // Desativa o watch mode para evitar reinícios constantes
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
